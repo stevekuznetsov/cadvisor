b3b192542a28f1bbfd069aae26e0ec69f3c0a289
Paweł Szulik <pawel.szulik@intel.com>
2022-01-29 18:42:07 +0100

    pages: Docker key value changer should be more generic.
    Signed-off-by: Paweł Szulik <pawel.szulik@intel.com>

--- cmd/internal/pages/docker.go
+++ cmd/internal/pages/docker.go
@@ -39,12 +39,12 @@ func toStatusKV(status info.DockerStatus) ([]keyVal, []keyVal) {
 		ds = append(ds, keyVal{Key: k, Value: v})
 	}
 	return []keyVal{
-		{Key: "Docker Version", Value: status.Version},
-		{Key: "Docker API Version", Value: status.APIVersion},
+		{Key: "Version", Value: status.Version},
+		{Key: "API Version", Value: status.APIVersion},
 		{Key: "Kernel Version", Value: status.KernelVersion},
 		{Key: "OS Version", Value: status.OS},
 		{Key: "Host Name", Value: status.Hostname},
-		{Key: "Docker Root Directory", Value: status.RootDir},
+		{Key: "Root Directory", Value: status.RootDir},
 		{Key: "Execution  Driver", Value: status.ExecDriver},
 		{Key: "Number of Images", Value: strconv.Itoa(status.NumImages)},
 		{Key: "Number of Containers", Value: strconv.Itoa(status.NumContainers)},
